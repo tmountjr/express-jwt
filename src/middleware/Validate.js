beb28aec4d133511319e90d3b8c6fbfadc569b9b
Tom Mount <tmountjr@gmail.com>
2025-01-24 18:38:57 -0500

    add comments

--- src/middleware/Validate.js
+++ src/middleware/Validate.js
@@ -8,7 +8,14 @@ const jwtSecret = process.env.JWT_SECRET
 const publicKey = KEYUTIL.getKey(readFile(path.join(__dirname, '..', '..', 'certs', 'pubkey.pem')).replace(/[\n\r]+/g, ''))
 
 const validate = function(req, res, next) {
+  // Get the token from the Authorization header.
   const token = req.headers['authorization'].replace('Bearer ', '')
+
+  // Alternatively, you can take it from a cookie; here we're looking for one called "jwt".
+  // const cookies = req.headers.cookie.split('; ')
+  // const token = cookies.find(c => /^jwt=/.test(c)).split('=')[1].trim()
+
+  // Get the token header, payload, and signing algorithm used
   const [ header, payload ] = token.split('.')
   const { alg } = JSON.parse(fromBase64Url(header))
 
@@ -17,10 +24,13 @@ const validate = function(req, res, next) {
 
   try {
     if (/^HS/.test(alg)) {
+      // HSxxx algorithms use a shared secret.
       validationComponent = jwtSecret
     } else if (/^[REP]S/.test(alg)) {
+      // RSxxx, ESxxx, and PSxxx algorithms all use a public key.
       validationComponent = publicKey
     } else {
+      // Those are the only options; if another one was specified, throw an error.
       console.log(`Invalid algorithm specified: ${alg}`)
       res.status(501).send('Not Implemented')
     }
@@ -29,12 +39,15 @@ const validate = function(req, res, next) {
     res.status(500)
   }
 
+  // Validate the JWT using the jsrsasign library.
   isValid = KJUR.jws.JWS.verifyJWT(token, validationComponent, { alg: [alg] })
 
   if (isValid) {
+    // Only if the signature is valid, decode the payload and add it to the request.
     req.payload = JSON.parse(fromBase64Url(payload))
     next()
   } else {
+    // If the signature is invalid, abort the request with a 401.
     res.status(401).send('Unauthorized')
   }
 }
